890a9b71493f17af7389dba8feeec2a7dd4b8a9d
Admin <Admin@DESKTOP-GG3HP9G>
2021-07-19 16:43:12 +0900

    마지막

--- Manager/src/co/jelly/manager/menu/AdminMenu.java
+++ Manager/src/co/jelly/manager/menu/AdminMenu.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import co.jelly.manager.board.service.BoardService;
 import co.jelly.manager.board.serviceImpl.BoardServiceImpl;
+import co.jelly.manager.board.vo.BoardVO;
 import co.jelly.manager.member.service.MemberService;
 import co.jelly.manager.member.serviceImpl.MemberServiceImpl;
 import co.jelly.manager.member.vo.MemberVO;
@@ -18,21 +19,214 @@ public class AdminMenu {
 	private void title() {
 		System.out.println("_______________________________________________");
 		System.out.println("		< 관리자 메뉴 >");
+		System.out.println("| 1.사용자 관리					|");
+		System.out.println("| 2.게시판 관리					|");
+		System.out.println("| 3.종    료 					|");
+		System.out.println("_______________________________________________");
+		System.out.print("원하는 작업 선택>> ");
 	}
 
-	private void adminMenu(MemberVO vo) {
+	private void subMemberTitle() {
+		System.out.println("_______________________________________________");
+		System.out.println("		< 사용자 관리 >");
+		System.out.println("| 1.멤버 목록 보기					|");
+		System.out.println("| 2.멤버 정보 조회					|");
+		System.out.println("| 3.멤버 정보 삭제					|");
+		System.out.println("| 4.돌아가기					|");
+		System.out.println("_______________________________________________");
+		System.out.print("원하는 작업 선택>> ");
+	}
+
+	private void subBoardTitle() {
+		System.out.println("_______________________________________________");
+		System.out.println("		< 게시판 관리 >");
+		System.out.println("| 1.글 목록 보기					|");
+		System.out.println("| 2.글 쓰기					|");
+		System.out.println("| 3.글 수정					|");
+		System.out.println("| 4.글 삭제					|");
+		System.out.println("| 5.돌아가기					|");
+		System.out.println("_______________________________________________");
+		System.out.print("원하는 작업 선택>> ");
+	}
+
+	private void adminMenu(MemberVO vo) { // 여기
 		System.out.println(vo.getName() + " 관리자님 환영합니다.");
-		title();
-		memberList();
+		boolean b = false;
+		do {
+			title(); // 주메뉴 출력
+			int choice;
+			switch (choice = sc.nextInt()) {
+			case 1:
+				userManager();
+				break;
+			case 2:
+				boardManager();
+				break;
+			case 3:
+				b = true;
+				System.out.println("작업을 종료합니다.");
+				break;
+			}
+		} while (!b);
 	}
 
-	private void memberList() {
-		// TODO 멤버 목록 가져오기
-		List<MemberVO> list = new ArrayList<MemberVO>();
-		list = memDao.memberSelectList();
+	private void boardManager() {
+		// TODO 게시판 관리
+		boolean b = false;
+		do {
+			int chk;
+			subBoardTitle();
+			switch (chk = sc.nextInt()) {
+			case 1:
+				boardSelectList(); // 글 목록
+				break;
+			case 2:
+				sc.nextLine();
+				boardInsert(); // 글 쓰기
+				break;
+			case 3:
+				sc.nextLine();
+				boardUpdate(); // 글 수정
+				break;
+			case 4:
+				sc.nextLine();
+				boardDelete(); // 글 삭제
+				break;
+			case 5:
+				b = true;
+				System.out.println("목록으로 돌아갑니다.");
+				break;
+			}
 
-		for (MemberVO vo : list) {
-			vo.toString();
+		} while (!b);
+	}
+
+	private void boardSelectList() {
+		// TODO 글 목록 조회
+		List<BoardVO> boards = new ArrayList<BoardVO>();
+		boards = boDao.boardSelectList();
+		for (BoardVO board : boards) {
+			board.toString();
+		}
+	}
+
+	private void boardInsert() {
+		// TODO 글 쓰기
+		BoardVO vo = new BoardVO();
+		System.out.print("게시판 ID 입력: ");
+		vo.setBoardid(sc.next());
+		sc.nextLine();
+		System.out.print("게시판 제목 입력: ");
+		vo.setTitle(sc.nextLine());
+		System.out.print("게시판 내용 입력: ");
+		vo.setSubject(sc.nextLine());
+		System.out.print("작성자 입력: ");
+		vo.setWriter(sc.next());
+		sc.nextLine();
+
+		int n = boDao.boardInsert(vo);
+
+		if (n != 0) {
+			System.out.println("정상적으로 입력되었습니다.");
+		} else {
+			System.out.println("! 입력 실패");
+		}
+	}
+
+	private void boardUpdate() {
+		// TODO 글 수정(내용만)
+		int n = 0;
+		BoardVO vo = new BoardVO();
+		System.out.print("수정할 게시글의 ID 입력: ");
+		vo.setBoardid(sc.nextLine());
+		System.out.print("수정할 내용 입력: ");
+		vo.setSubject(sc.nextLine());
+
+		n = boDao.boardUpdate(vo);
+
+		if (n != 0) {
+			System.out.println("정상적으로 수정되었습니다.");
+		} else {
+			System.out.println("! 수정 실패");
+		}
+
+	}
+
+	private void boardDelete() {
+		// TODO 글 삭제
+		int n = 0;
+		BoardVO vo = new BoardVO();
+		System.out.print("삭제할 게시글의 ID 입력: ");
+		vo.setBoardid(sc.nextLine());
+
+		n = boDao.boardDelete(vo);
+
+		if (n != 0) {
+			System.out.println("정상적으로 삭제되었습니다.");
+		} else {
+			System.out.println("! 삭제 실패");
+		}
+
+	}
+
+	private void userManager() {
+		// TODO 사용자 관리
+		boolean b = false;
+		do {
+			int chk;
+			subMemberTitle(); // 사용자 메뉴 출력
+			switch (chk = sc.nextInt()) {
+			case 1:
+				memberSelectList(); // 멤버목록 조회
+				break;
+			case 2:
+				sc.nextLine();
+				memberSelect(); // 멤버 조회
+				break;
+			case 3:
+				sc.nextLine();
+				memberDelete(); // 멤버 삭제
+				break;
+			case 4:
+				b = true;
+				System.out.println("목록으로 돌아갑니다.");
+				break;
+			}
+		} while (!b);
+	}
+
+	private void memberDelete() {
+		// TODO 멤버 삭제
+		MemberVO member = new MemberVO();
+		System.out.print("삭제할 ID를 입력>>> ");
+		member.setId(sc.nextLine());
+		int n = memDao.memberDelete(member);
+		if (n != 0) {
+			System.out.println("정상적으로 삭제되었습니다.");
+		} else {
+			System.out.println("! 삭제 실패");
+		}
+
+	}
+
+	private void memberSelect() {
+		// TODO 멤버 조회
+		MemberVO member = new MemberVO();
+		System.out.print("검색할 ID를 입력>>> ");
+		member.setId(sc.nextLine());
+		member = memDao.memberSelect(member);
+
+		member.toString();
+	}
+
+	private void memberSelectList() {
+		// TODO 멤버 목록 조회
+		System.out.println("		- 게시판 관리 -");
+
+		List<MemberVO> members = new ArrayList<MemberVO>();
+		members = memDao.memberSelectList();
+		for (MemberVO member : members) {
+			member.toString();
 		}
 	}
 
--- Manager/src/co/jelly/manager/menu/UserMenu.java
+++ Manager/src/co/jelly/manager/menu/UserMenu.java
@@ -19,21 +19,116 @@ public class UserMenu {
 	private void title() {
 		System.out.println("_______________________________________________");
 		System.out.println("		< 사용자 메뉴 >");
+		System.out.println("| 1.나의 정보 수정하기				|");
+		System.out.println("| 2.게시글 보기					|");
+		System.out.println("| 3.게시글 작성					|");
+		System.out.println("| 4.게시글 수정					|");
+		System.out.println("| 5.종     료					|");
+		System.out.println("_______________________________________________");
+		System.out.print("원하는 작업 선택>> ");
 	}
 
-	private void userMenu(MemberVO vo) {
+	private void userMenu(MemberVO vo) { // 여기
 		System.out.println(vo.getName() + "님 환영합니다.");
-		title();
-		boardList();
+		boolean b = false;
+		do {
+			title();
+			int choice;
+			switch (choice = sc.nextInt()) {
+			case 1:
+				sc.nextLine();
+				editMyInfo(vo);
+				break;
+			case 2:
+				showBoard();
+				break;
+			case 3:
+				sc.nextLine();
+				writeBoard(vo);
+				break;
+			case 4:
+				sc.nextLine();
+				editBoard(vo.getId());
+				break;
+			case 5:
+				b = true;
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+		} while (!b);
+
 	}
 
-	private void boardList() {
-		// TODO Auto-generated method stub
+	private void editMyInfo(MemberVO vo) {
+		// TODO 내 정보 수정
+		int n = 0;
+		System.out.print("수정할 전화번호 입력: ");
+		vo.setTel(sc.nextLine());
+		System.out.print("수정할 주소 입력: ");
+		vo.setAddress(sc.nextLine());
+
+		n = memDao.memberUpdate(vo);
+		if (n != 0) {
+			System.out.println("내 정보가 정상적으로 수정되었습니다.");
+		} else {
+			System.out.println("! 내 정보 수정 실패");
+		}
+	}
+
+	private void showBoard() {
+		// TODO 게시글 보기
 		List<BoardVO> list = new ArrayList<BoardVO>();
 		list = boDao.boardSelectList();
 
-		for (BoardVO vo : list) {
-			vo.toString();
+		for (BoardVO board : list) {
+			board.toString();
+		}
+	}
+
+	private void writeBoard(MemberVO vo) {
+		// TODO 게시글 작성
+		int n = 0;
+		BoardVO board = new BoardVO();
+		System.out.print("작성할 글의 ID 입력: ");
+		board.setBoardid(sc.nextLine());
+		board.setWriter(vo.getId());
+		System.out.print("작성할 글의 제목 입력: ");
+		board.setTitle(sc.nextLine());
+		System.out.print("작성할 글의 내옹 입력: ");
+		board.setSubject(sc.nextLine());
+		n = boDao.boardInsert(board);
+
+		if (n != 0) {
+			System.out.println("글이 정상적으로 작성되었습니다.");
+		} else {
+			System.out.println("! 글 작성 실패");
+		}
+	}
+
+	private void editBoard(String id) {
+		// TODO 게시글 수정
+		int n = 0;
+		BoardVO board = new BoardVO();
+		System.out.print("수정할 글의 ID 입력: ");
+		String boardId = sc.nextLine();
+		board.setBoardid(boardId);
+		board = boDao.boardSelect(board);	//id에 맞는 글을 선택한다.
+		if(board.getWriter().equals(id)) {
+			board = new BoardVO();
+			System.out.print("수정할 글의 내용 입력: ");
+			board.setSubject(sc.nextLine());
+			board.setWriter(id);
+			board.setBoardid(boardId);
+			n = boDao.boardUpdate(board);
+			if(n != 0) {
+				System.out.println("정상적으로 글이 수정되었습니다.");
+			} else {
+				System.out.println("! 글 수정 실패");
+			}
+		}
+		else {
+			System.out.println("본인의 글만 수정 가능합니다.");
+			board.toString();
 		}
 	}
 
